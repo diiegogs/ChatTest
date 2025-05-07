35769b81c7b56cd3b038d34cf6ef71b81831d83f
JUAN DIEGO GARCIA SERRANO <diiegogarcs26@gmail.com>
2025-05-07 14:08:52 -0600

    Initial Commit

---
+++ ChatTest/ChatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatTestApp.swift
+//  ChatTest
+//
+//  Created by Juan Diego Garcia Serrano on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
